ac1d4bd9672f2a45a95378884935ebd7c613228f
kotvaska <zolotykh24@gmail.com>
2018-06-28 13:13:07 +0500

    Added image cache

---
+++ TinkoffPay/data/models/ImageStorageError.swift
@@ -0,0 +1,11 @@
+//
+// Created by Anastasia Zolotykh on 28.06.2018.
+// Copyright (c) 2018 kotvaska. All rights reserved.
+//
+
+import Foundation
+
+struct ImageStorageError: Error {
+    let title: String
+    let message: String
+}
--- TinkoffPay/mvp/interactor/ImageInteractor.swift
+++ TinkoffPay/mvp/interactor/ImageInteractor.swift
@@ -4,15 +4,75 @@
 //
 
 import Foundation
+import UIKit
 
 class ImageInteractor {
 
-    func clearIcons(_ completion: @escaping () -> ()) {
+    func clearIcon(iconName: String) {
+        let fileManager = FileManager.default
+        let nsDocumentDirectory = FileManager.SearchPathDirectory.documentDirectory
+        let nsUserDomainMask = FileManager.SearchPathDomainMask.userDomainMask
+        let paths = NSSearchPathForDirectoriesInDomains(nsDocumentDirectory, nsUserDomainMask, true)
+        guard let _ = paths.first else {
+            return
+        }
 
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let fileURL = documentsURL.appendingPathComponent(iconName)
+
+        do {
+            try fileManager.removeItem(atPath: fileURL.path)
+        } catch let error as NSError {
+            print("kotvaska --- clear icon error: \(error.debugDescription)")
+        } catch {
+            print("kotvaska --- clear icon error")
+        }
     }
 
-    func getPartnersIcons(_ completion: ([Partner], Error?) -> ()) {
+    func getIcon(iconName: String, _ completion: (UIImage?, Error?) -> ()) {
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let fileUrl = documentsURL.appendingPathComponent(iconName)
+        if FileManager.default.fileExists(atPath: fileUrl.path) {
+            do {
+                let imageData = try Data(contentsOf: fileUrl)
+                let image = UIImage(data: imageData)
+                completion(image, nil)
 
+            } catch let e {
+                completion(nil, e)
+            }
+        } else {
+            completion(nil, ImageStorageError(title: "Ошибка", message: "Изображение не найдено"))
+        }
+    }
+
+    func saveIcon(iconName: String, oldUrl: URL) -> Data? {
+        if FileManager.default.fileExists(atPath: oldUrl.path) {
+            do {
+                let imageData = try Data(contentsOf: oldUrl)
+                if let image = UIImage(data: imageData) {
+                    saveIcon(iconName: iconName, image: image)
+                    return imageData
+                }
+
+            } catch let e {
+                print("kotvaska --- save icon error: \(e.localizedDescription)")
+                return nil
+            }
+        }
+        return nil
+    }
+
+    func saveIcon(iconName: String, image: UIImage) {
+        do {
+            let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            let fileURL = documentsURL.appendingPathComponent(iconName)
+            let data = UIImageJPEGRepresentation(image, 1.0)
+            try data?.write(to: fileURL, options: .atomic)
+
+        } catch {
+
+        }
     }
 
 }
